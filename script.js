2da4a0924d08a0edef3eae53a46f29ae4574e82f
DrianDevs <adriangm8110@gmail.com>
2025-02-23 13:10:58 +0100

    Slider Video Avanzado, Header retocado

--- script.js
+++ script.js
@@ -6,19 +6,31 @@ document.addEventListener("DOMContentLoaded", () => {
     window.addEventListener("scroll", () => {
       if (window.scrollY >= 56) {
         nav.classList.add("navbar-scrolled");
-        nav.classList.remove("navbar-dark");
+        // nav.classList.remove("navbar-dark");
 
-        logo.src = "/images/pr-lovers-black-logo.png";
+        // logo.src = "/images/pr-lovers-black-logo.png";
       } else if (window.scrollY < 56) {
         nav.classList.remove("navbar-scrolled");
-        nav.classList.add("navbar-dark");
+        // nav.classList.add("navbar-dark");
 
-        logo.src = "/images/pr-lovers-white-logo.png";
+        // logo.src = "/images/pr-lovers-white-logo.png";
       }
     });
   } else {
     console.error("Navbar element not found");
   }
 
+  //Javascript para el video slider
+
   const btns = document.querySelectorAll(".nav-btn");
+
+  var sliderNav = function (manual) {
+    btns[manual].classList.add("active");
+  };
+
+  btns.forEach((btn, i) => {
+    btn.addEventListener("click", () => {
+      sliderNav(i);
+    });
+  });
 });
